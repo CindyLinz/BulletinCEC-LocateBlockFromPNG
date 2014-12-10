bf76dbee833317fac742535ab1a1fc5d524673ca
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2014-12-11 00:40:42 +0800

    guess field

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # PURPOSE
 
-把中選會競選公報底圖裡的「格子」找出來
++ locate\_block: 把中選會競選公報底圖裡的「格子」找出來
++ guess\_field.pl: 根據「格子」的坐標分佈去猜是不是候選人資料以及內容應該是什麼
 
 # DEPENDENCY
 
 + libpng
++ Perl
 
 # INSTALL
 
@@ -16,19 +18,55 @@ $ make
 
 ```
 $ ./locate_block < input.png > output.txt
+$ perl guess_field.pl < output.txt > output.json
+```
+or
+```
+$ ./locate_block < input.png | perl guess_field.pl > output.json
 ```
 
 # OUTPUT FORMAT
 
+output.txt
 ```
 x1 y1 width1 height1
 x2 y2 width2 height2
 ...
 ```
 
+output.json (如果猜得正確的話, 會依該頁公報中候選人的順序排列)
+```
+[ { "號次":
+    [x, y, width, height]
+  , "相片":
+    [x, y, width, height]
+  , "姓名":
+    [x, y, width, height]
+  , "出生年月日":
+    [x, y, width, height]
+  , "性別":
+    [x, y, width, height]
+  , "出生地":
+    [x, y, width, height]
+  , "推薦之政黨":
+    [x, y, width, height]
+  , "學歷":
+    [x, y, width, height]
+  , "經歷":
+    [x, y, width, height]
+  , "政見":
+    [x, y, width, height]
+  }
+, { ...
+  }
+...
+]
+```
+
 # SAMPLE
 + [sample input](https://github.com/CindyLinz/BulletinCEC-LocateBlockFromPNG/blob/master/input.png)
-+ [sample output](https://github.com/CindyLinz/BulletinCEC-LocateBlockFromPNG/blob/master/output.txt)
++ [sample output (txt)](https://github.com/CindyLinz/BulletinCEC-LocateBlockFromPNG/blob/master/output.txt)
++ [sample output (json)](https://github.com/CindyLinz/BulletinCEC-LocateBlockFromPNG/blob/master/output.json)
 
 # LICENSE
 
---
+++ guess_field.pl
@@ -0,0 +1,130 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use List::Util qw(reduce max min);
+
+my @field = qw(
+    號次
+    相片
+    姓名
+    出生年月日
+    性別
+    出生地
+    推薦之政黨
+    學歷
+    經歷
+    政見
+);
+my $field_num = @field;
+
+my $input = do {
+    local $/;
+    <STDIN>;
+};
+
+my @block;
+while( $input =~ /(\d+)\D+(\d+)\D+(\d+)\D+(\d+)/g ) {
+    push @block, [$1, $2, $3, $4];
+}
+@block = sort { $a->[1] <=> $b->[1] } @block;
+
+my @row;
+while( @block ) {
+    my $y0 = $block[0][1];
+    my @new_row;
+    while( @block && $block[0][1] - $y0 <= 15 ) {
+        push @new_row, shift @block;
+    }
+    push @row, \@new_row;
+}
+
+for my $row (@row) {
+    my $line_width = 10000;
+    my $i = 0;
+    while( $i < $#{$row} ) {
+        if( $row->[$i][0] + $row->[$i][2] < $row->[$i+1][0] ) {
+            $line_width = min($line_width, $row->[$i+1][0] - ($row->[$i][0]+$row->[$i][2]));
+        }
+        ++$i;
+    }
+
+    #print "y = $row->[0][1]\n";
+    #print "line_width = $line_width\n";
+
+    next if( $line_width > 10 );
+
+    $i = 0;
+    while( $i < $#{$row} ) {
+        if( $row->[$i][0] + $row->[$i][2] + $line_width * 2 + 15 < $row->[$i+1][0] ) {
+            splice $row, $i+1, 0, [
+                $row->[$i][0] + $row->[$i][2] + $line_width, # x
+                $row->[$i][1], #y
+                $row->[$i+1][0] - ($row->[$i][0] + $row->[$i][2]) - $line_width * 2, # w
+                0,
+            ];
+            #print "fix @{$row->[$i+1]}\n";
+            ++$i;
+        }
+        ++$i;
+    }
+}
+
+my @heading_row;
+for my $row (@row) {
+    @$row = sort { $a->[0] <=> $b->[0] } @$row;
+    if( $field_num <= @$row && @$row <= $field_num+2 ) {
+        shift $row while( @$row > $field_num && $row->[1][2] < $row->[0][2] * 2.5 );
+        pop $row while( @$row > $field_num );
+        push @heading_row, $row;
+    } elsif( 2*$field_num <= @$row && @$row <= 2*($field_num+2) ) {
+        shift $row while( @$row > $field_num && $row->[1][2] < $row->[0][2] * 2.5 );
+        push @heading_row, [splice $row, 0, $field_num];
+
+        shift $row while( @$row > $field_num && $row->[1][2] < $row->[0][2] * 2.5 );
+        pop $row while( @$row > $field_num );
+        push @heading_row, $row;
+    }
+}
+
+my @final_row;
+for my $row (@heading_row) {
+    my $height = reduce { max $a, $b->[3] } 0, @$row;
+    if( $height > 230 ) {
+        for my $b (@$row) {
+            $b->[3] = $height;
+        }
+        push @final_row, $row;
+    }
+}
+@final_row = sort {
+    abs( $a->[0][0] - $b->[0][0] ) > 50 && $a->[0][0] <=> $b->[0][0] || $a->[0][1] <=> $b->[0][1]
+} @final_row;
+
+if( @final_row ) {
+    for my $row (@final_row) {
+        for my $i (0..$#field) {
+            if( $i==0 ) {
+                if( \$row == \$final_row[0] ) {
+                    print "[ { ";
+                } else {
+                    print ", { ";
+                }
+            } else {
+                print "  , ";
+            }
+            for my $j (0..3) {
+                $row->[$i][$j] ||= 0;
+            }
+            print qq("$field[$i]":\n    [$row->[$i][0], $row->[$i][1], $row->[$i][2], $row->[$i][3]]\n);
+        }
+        print "  }\n";
+    }
+    print "]\n";
+} else {
+    print "[]\n";
+}
+
+#print 'heading_row=', 0+@heading_row, $/;
+#print 'final_row=', 0+@final_row, $/;
